217294ca853223acb8de68a6ebd1c36c22a68d9c
Takuma Sato <sasakama057@outlook.com>
2025-07-11 22:41:11 +0900

    Update MainTask

--- TodoApp/Models/MainTask.cs
+++ TodoApp/Models/MainTask.cs
@@ -5,79 +5,85 @@ namespace TodoApp.Models
     public class MainTask : IValidatableObject
     {
         [Key]
-        public int Id { get; set; } // PK
+        public int Id { get; set; } // PK.
 
         [Required]
-        public int UserId { get; set; } // FK to User
+        public int UserId { get; set; } // FK: User.Id.
 
         [Required(ErrorMessage = "タスク名は必須項目です。")]
-        [Display(Name = "タスク名")]
-        [StringLength(100, MinimumLength = 1, ErrorMessage = "タスク名は1～255文字以内で入力してください。")]
+        [Display(Name = "大タスク名")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "大タスク名は1～100文字以内で入力してください。")]
         public string Title { get; set; }
 
         [Required]
-        [Display(Name = "進捗")]
-        [Range(0.0, 100.0, ErrorMessage = "Progress rate takes 0-100.")]
-        public double Progress { get; set; } // Progress percentage (0-100)
+        [Display(Name = "進捗度")]
+        [Range(0, 100, ErrorMessage = "進捗度の値が不正です。")]
+        public int Progress { get; set; }
 
-        [Required(ErrorMessage = "優先度は必須項目です。")]
-        [Display(Name = "優先度")]
-        [Range(0, 255, ErrorMessage = "Priority takes 0-255.")]
-        public int Priority { get; set; } // Priority level (0:Low, 1:Medium, 2～:High)
+        [Required(ErrorMessage = "緊急度は必須項目です。")]
+        [Display(Name = "緊急度")]
+        [Range(1, 3, ErrorMessage = "緊急度の値が不正です。")]
+        public int Urgency { get; set; } // (0:低, 1:中, 2:高).
 
-        [Required(ErrorMessage = "開始時刻は必須項目です。")]
-        [Display(Name = "開始時刻")]
+        [Required(ErrorMessage = "重要度は必須項目です。")]
+        [Display(Name = "重要度")]
+        [Range(1, 3, ErrorMessage = "重要度の値が不正です。")]
+        public int Importance { get; set; } // (0:低, 1:中, 2:高).
+
+        [Required(ErrorMessage = "開始日時は必須項目です。")]
+        [Display(Name = "開始日時")]
         [DataType(DataType.DateTime)]
-        public DateTime StartTime { get; set; }
+        public DateTime StartedAt { get; set; }
 
-        [Required(ErrorMessage = "終了時刻は必須項目です。")]
-        [Display(Name = "終了時刻")]
+        [Required(ErrorMessage = "終了日時は必須項目です。")]
+        [Display(Name = "終了日時")]
         [DataType(DataType.DateTime)]
-        public DateTime EndTime { get; set; } // Due date for the task
+        public DateTime EndedAt { get; set; } // 期日.
 
-        // カスタムバリデーション
+        // 開始・終了日時のカスタムバリデーション.
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             var result = new List<ValidationResult>();
 
-            // 開始時刻が現時刻以降であることをチェック
-            if (StartTime.Date < DateTime.Today)
+            // 開始日時が現在時刻以降であることをチェック.
+            if (StartedAt.Date < DateTime.Today)
             {
                 result.Add(new ValidationResult(
-                    "開始日は今日以降の日付を指定してください",
-                    new[] { nameof(StartTime) }
+                    "開始日時は現在時刻以降の日時を指定してください。",
+                    new[] { nameof(StartedAt) }
                 ));
             }
-            // 終了時刻が開始時刻以降であることをチェック
-            if (EndTime < StartTime)
+            // 終了日時が開始日時以降であることをチェック.
+            if (EndedAt < StartedAt)
             {
                 result.Add(new ValidationResult(
-                    "終了時刻は開始時刻以降の時刻を入力してください。",
-                    new[] { nameof(EndTime) }
+                    "終了日時は開始日時以降の日時を指定してください。",
+                    new[] { nameof(EndedAt) }
                 ));
             }
             return result;
         }
 
         [Display(Name = "備考")]
-        [StringLength(1024, ErrorMessage = "備考は1024文字以内で入力してください。")]
+        [StringLength(1000, ErrorMessage = "備考は1000文字以内で入力してください。")]
         public string? Description { get; set; }
 
+        [Display(Name = "完了")]
+        public bool IsCompleted { get; set; } = false; // チェックボックス用の完了フラグ.
 
 
-        [Display(Name = "作成日")]
+
+        [Display(Name = "作成日時")]
         [DataType(DataType.DateTime)]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-        [Display(Name = "最終更新日")]
+        [Display(Name = "最終更新日時")]
         [DataType(DataType.DateTime)]
         public DateTime? UpdatedAt { get; set; }
-        [Display(Name = "完了日")]
+        [Display(Name = "完了日時")]
         [DataType(DataType.DateTime)]
         public DateTime? CompletedAt { get; set; }
-        [Display(Name = "削除日")]
+        [Display(Name = "削除日時")]
         [DataType(DataType.DateTime)]
         public DateTime? DeletedAt { get; set; }
-        public bool IsCompleted { get; set; } = false;
-        public bool IsDeleted { get; set; } = false;
     }
 }
